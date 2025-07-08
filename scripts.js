2362d7160ee19b76ff28a122d4e0f24e4df146b1
Efraim Rocha <95988659+Efrals@users.noreply.github.com>
2025-07-08 17:32:22 -0300

    Finalizado animação e começando back-end

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function criarAnimacao() {
+    let textoInput = document.querySelector(".input-animacao").value
+
+    // Procura por o botão de animação
+    let button = document.querySelector(".botao-animacao")
+
+    // Desabilita botão temporariamente para esperar o retorno da IA
+    button.disabled = true
+}
