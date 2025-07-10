fdb8bac8abc2e8ac621038248f2a342649b4bb61
Efraim Rocha <95988659+Efrals@users.noreply.github.com>
2025-07-09 23:55:43 -0300

    primeiros passos para fazer a integração com a n8n

--- scripts.js
+++ scripts.js
@@ -1,9 +1,29 @@
 function criarAnimacao() {
-    let textoInput = document.querySelector(".input-animacao").value
+  let textoInput = document.querySelector(".input-animacao").value;
 
-    // Procura por o botão de animação
-    let button = document.querySelector(".botao-animacao")
+  // Procura por o botão de animação
+  let button = document.querySelector(".botao-animacao");
 
-    // Desabilita botão temporariamente para esperar o retorno da IA
-    button.disabled = true
+  // Desabilita botão temporariamente para esperar o retorno da IA
+  button.disabled = true;
 }
+
+let webhook = function criarAnimacao() {
+  let textoInput = document.querySelector(".input-animacao").value;
+
+  // Procura por o botão de animação
+  let button = document.querySelector(".botao-animacao");
+
+  // Desabilita botão temporariamente para esperar o retorno da IA
+  button.disabled = true;
+
+  let resposta = fetch(webhook, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      pergunta: textoInput,
+    }),
+  });
+};
