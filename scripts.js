58a864d213466b96693671732d3ed7622dfa06e9
Efraim Rocha <95988659+Efrals@users.noreply.github.com>
2025-07-11 18:03:35 -0300

    conexão inicial com ia criada e testada

--- scripts.js
+++ scripts.js
@@ -1,29 +1,27 @@
-function criarAnimacao() {
+let webhook = "https://efrals.app.n8n.cloud/webhook-test/animacao-css";
+
+// Função para criar a animação
+// Função assíncrona
+async function criarAnimacao() {
   let textoInput = document.querySelector(".input-animacao").value;
 
-  // Procura por o botão de animação
-  let button = document.querySelector(".botao-animacao");
+  let resposta = await fetch(webhook, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ pergunta: textoInput }),
+  });
 
-  // Desabilita botão temporariamente para esperar o retorno da IA
-  button.disabled = true;
+  // Pega a resposta e transforma em JSON
+  // await faz com que a função aguarde a resposta da API
+  let resultado = await resposta.json();
+
+  console.log(resultado);
 }
 
-let webhook = function criarAnimacao() {
-  let textoInput = document.querySelector(".input-animacao").value;
-
+/*
   // Procura por o botão de animação
   let button = document.querySelector(".botao-animacao");
 
   // Desabilita botão temporariamente para esperar o retorno da IA
   button.disabled = true;
-
-  let resposta = fetch(webhook, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      pergunta: textoInput,
-    }),
-  });
-};
+*/
