0471e6f1196ebdb4b77432b4e933ba4235ca5eed
Efraim Rocha <95988659+Efrals@users.noreply.github.com>
2025-07-14 17:40:24 -0300

    filtro para ordenar retorno da resposta e alguns comentários

--- scripts.js
+++ scripts.js
@@ -6,14 +6,20 @@ async function criarAnimacao() {
   let textoInput = document.querySelector(".input-animacao").value;
 
   let resposta = await fetch(webhook, {
+    // Método POST para enviar dados
     method: "POST",
+    // Avisa que você está enviando dados
     headers: { "Content-Type": "application/json" },
+    // Transforma o texto em JSON
     body: JSON.stringify({ pergunta: textoInput }),
   });
 
   // Pega a resposta e transforma em JSON
   // await faz com que a função aguarde a resposta da API
   let resultado = await resposta.json();
+
+  // Filtra e ordena todo retorno que está na variável resposta após a espera na variável resultado
+  let info = JSON.parse(resultado.resposta);
 
   console.log(resultado);
 }
